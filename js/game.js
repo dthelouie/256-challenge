2ec734e31dcb8a7109963d0940810c3ff1f0ae5f
Devin Mandelbaum & David Louie & Liam Mackey & Patrick Oliphant <pair+dmandelb.dthelouie.liam-mackey.poliphant@devbootcamp.com>
2016-02-25 15:34:31 -0500

    Add all movement options and spawning new tiles

--- js/game.js
+++ js/game.js
@@ -13,9 +13,26 @@ function Game(string){
   this.board = boardArr
 }
 
+Game.prototype.spawn = function(){
+  var row = Math.ceil(Math.random() * 4) - 1;
+  var item = Math.ceil(Math.random() * 4) - 1;
+  var random = Math.random();
+  var rand_num;
+  if (random < 0.8){
+    rand_num = 2;
+  } else {
+    rand_num = 4;
+  }
+  if (this.board[row][item] == 0){
+    this.board[row][item] = rand_num;
+  } else {
+    this.spawn();
+  }
+
+}
+
 Game.prototype.left = function(){
   this.move();
-  this.output();
 }
 
 Game.prototype.right = function(){
@@ -26,32 +43,29 @@ Game.prototype.right = function(){
   for (var i=0; i<this.board.length; i++){
     this.board[i].reverse();
   }
-  this.output();
 }
 
 Game.prototype.up = function(){
   for (var i=0; i<this.board.length; i++){
     this.board[i].reverse()
   }
-  this.down();
+  this.board = _.zip(this.board[0], this.board[1], this.board[2], this.board[3])
+  this.left();
+  this.board = _.zip(this.board[0], this.board[1], this.board[2], this.board[3])
+  this.output();
   for (var i=0; i<this.board.length; i++){
     this.board[i].reverse()
   }
-  this.output();
 }
 
 Game.prototype.down = function(){
-  for (var i=0; i<this.board.length; i++){
-    _.zip(this.board[i])
-  }
-  this.move();
-  for (var i=0; i<this.board.length; i++){
-    _.zip(this.board[i])
-  }
-  this.output();
+  this.board = _.zip(this.board[0], this.board[1], this.board[2], this.board[3])
+  this.right();
+  this.board = _.zip(this.board[0], this.board[1], this.board[2], this.board[3])
 }
 
 Game.prototype.move = function(){
+  this.spawn();
   for (var i = 0; i < 4; i++){
     var new_row = _.compact(this.board[i])
     // var new_row = this.board[i].map(function(num){
@@ -59,14 +73,14 @@ Game.prototype.move = function(){
     //     return num;
     //   }
     // })
-    for (var i = 0; i < new_row.length; i++){
-      if (new_row[i] == new_row[i+1]){
-        new_row[i] = new_row[i] + new_row[i+1]
-        new_row[i+1] = 0
+    for (var j = 0; j < new_row.length; j++){
+      if (new_row[j] == new_row[j+1]){
+        new_row[j] = new_row[j] + new_row[j+1]
+        new_row[j+1] = 0
       }
       new_row = _.compact(new_row)
     }
-    while (new_row.length <= 4){
+    while (new_row.length < 4){
       new_row.push(0)
     }
     this.board[i] = new_row
@@ -81,8 +95,8 @@ Game.prototype.output = function(){
 
 
 
-game = new Game('2000200000000000');
+// game = new Game('2000200000000000');
 
 
-game.output();
-game.up();
+// game.output();
+// game.up();
