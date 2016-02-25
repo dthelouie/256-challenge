dc7f18d0e18f9da5ce333d7abfad06bb12e1380c
Devin Mandelbaum & David Louie & Liam Mackey & Patrick Oliphant <pair+dmandelb.dthelouie.liam-mackey.poliphant@devbootcamp.com>
2016-02-25 18:29:16 -0500

    DRY out some code and rearrange spawning to be much less labor-intensive for the processor

--- js/game.js
+++ js/game.js
@@ -3,29 +3,30 @@ function Game(string){
   for (var i = 0; i < fullArr.length; i++){
     fullArr[i] = parseInt(fullArr[i]);
   };
-  // fullArr.forEach(function(num){
-  //   num = parseInt(num)
-  // })
+  this.boardSize = Math.sqrt(fullArr.length);//figure out error handling if boardSize is not an integer
   var boardArr = []
-  for (var i = 0; i < fullArr.length; i += 4){
-    boardArr.push(fullArr.slice(i, i+4));
+  for (var i = 0; i < fullArr.length; i += this.boardSize){
+    boardArr.push(fullArr.slice(i, i+this.boardSize));
   }
   this.board = boardArr
 }
 
 Game.prototype.spawn = function(){
-  //danger
-  var row = Math.ceil(Math.random() * 4) - 1;
-  var item = Math.ceil(Math.random() * 4) - 1;
-  var random = Math.random();
+  var zero_arr = [];
+  for (var y = 0; y < this.board.length; y++) {
+    for (var x = 0; x < this.board[y].length; x++) {
+      if (this.board[y][x] == 0) {zero_arr.push([y,x])}
+    }
+  };
+  var coordinates = _.sample(zero_arr)
   var rand_num;
-  if (random < 0.8){
+  if (Math.random() < 0.8){
     rand_num = 2;
   } else {
     rand_num = 4;
   }
-  if (this.board[row][item] == 0){
-    this.board[row][item] = rand_num;
+  if (this.board[coordinates[0]][coordinates[1]] == 0){
+    this.board[coordinates[0]][coordinates[1]] = rand_num;
   } else if (this.movable == true){
     return true
   } else if (this.movable == false){
@@ -46,7 +47,7 @@ Game.prototype.right = function(){
   this.move();
   for (var i=0; i<this.board.length; i++){
     this.board[i].reverse();
-  }//maybe WET wrapper for revers?
+  }
 }
 
 Game.prototype.up = function(){
@@ -54,9 +55,8 @@ Game.prototype.up = function(){
     this.board[i].reverse()
   }
   this.board = _.zip(this.board[0], this.board[1], this.board[2], this.board[3])
-  this.left();//change to move for readablity
+  this.move();
   this.board = _.zip(this.board[0], this.board[1], this.board[2], this.board[3])
-  this.output();//?
   for (var i=0; i<this.board.length; i++){
     this.board[i].reverse()
   }
@@ -69,28 +69,22 @@ Game.prototype.down = function(){
 }
 
 Game.prototype.move = function(){
-  for (var i = 0; i < 4; i++){
+  for (var i = 0; i < this.boardSize; i++){
     var new_row = _.compact(this.board[i])
-    // var new_row = this.board[i].map(function(num){
-    //   if (num != 0){
-    //     return num;
-    //   }
-    // })
     for (var j = 0; j < new_row.length; j++){
       if (new_row[j] == new_row[j+1]){
         new_row[j] = new_row[j] + new_row[j+1]
-        new_row[j+1] = 0//remove it? or jump it?// look into splice
+        new_row.splice(j+1,1);
       }
-      new_row = _.compact(new_row)//WET
     }
-    while (new_row.length < 4){
+    while (new_row.length < this.boardSize){
       new_row.push(0);
     }
     this.board[i] = new_row;
   }
   this.spawn();
   this.movable()
-}// :/
+}
 
 Game.prototype.output = function(){
   for (var i = 0; i < this.board.length; i++){
@@ -103,7 +97,7 @@ Game.prototype.movable = function(){
     if (_.find(this.board[n], function(num){ return num == 0})){
       return true;
     } else {
-      for (var x = 1; x < 4; x ++){
+      for (var x = 1; x < this.boardSize; x ++){
         if (this.board[n][x] == this.board[n][x-1]){
           return true;
         }
@@ -113,10 +107,3 @@ Game.prototype.movable = function(){
   console.log('game over!')
   return false;
 }
-
-
-// game = new Game('2000200000000000');
-
-
-// game.output();
-// game.up();
